fa7d64a4b405be87ac6c4a491ab5684b2d796da0
rivasjoaco <rivasjoaco@gmail.com>
2026-09-01 11:28:12 -0300

    app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ CENTROS_CONFIG = {
     "Córdoba (Centro 0960)": {
         "codigos": ["0960", "960"],
         "depot_address": "Depósito San Isidro EDASA Coca Cola X5016 Córdoba, Argentina",
-        "depot_coords": (-31.38495, -64.08843),
+        "depot_coords": (-31.384944, -64.088444),
         "provincia": "Córdoba",
         "ciudad": "Córdoba Capital"
     },
